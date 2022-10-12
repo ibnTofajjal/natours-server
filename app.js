e2ffc7843fc9a32fa49f1f6bffb72854bdda50db
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2022-10-13 03:10:26 +0600

    post new tour with sending unique id

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// Get API
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -14,6 +18,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+// Post API
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 // ------------------------------
