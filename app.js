31c37d077953dacdbd61c71d95806eade35f2d0f
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2022-10-13 02:37:56 +0600

    read file from fileSystem

---
+++ app.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+// ------------------------------
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server Running On PORT ${PORT}`);
+});
