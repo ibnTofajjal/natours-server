0c44a08a06eb8afd07cf406274877fb59a521c65
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2022-10-13 03:31:29 +0600

    update by patch

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
   const tour = tours.find((el) => el.id === id);
   if (!tour) {
     return res.status(404).json({
-      status: 'Failde',
+      status: 'FAILED',
       message: 'Invalid ID',
     });
   }
@@ -58,6 +58,23 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+//  Update By ID [Patch]
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'FAILED',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: `[UPDATED TOUR HERE]`,
+    },
+  });
 });
 
 // ------------------------------
