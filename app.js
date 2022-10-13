06574bd53ded16e681d591593e6aefda2eaf767b
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2022-10-13 16:28:55 +0600

    use our own[middlewares]

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+
+// MIDDLEWARES =============================
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Hello from the middleware ✋');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// ROUTES HANDLERS ===========================
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestTime: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -87,6 +102,7 @@ const deleteTour = (req, res) => {
   });
 };
 
+// ROUTES ===========================
 app.route('/api/v1/tours').get(getAllTours).post(creatTour);
 app
   .route('/api/v1/tours/:id')
